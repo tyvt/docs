a8e95df9dab7383207bd5b2188b74f3fdd8ef456
ty <you@example.com>
2024-06-21 18:03:08 +0800

    update version

--- src/config.js
+++ src/config.js
@@ -21,13 +21,6 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES)
 
 export const GITHUB_EDIT_URL = `https://github.com/javascriptfield/docs/edit/master`
 
-// See "Algolia" section of the README for more information.
-export const ALGOLIA = {
-	indexName: 'XXXXXXXXXX',
-	appId: 'XXXXXXXXXX',
-	apiKey: 'XXXXXXXXXX',
-}
-
 
 export const SIDEBAR = {
 	zh: {
